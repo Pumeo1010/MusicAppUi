268e2929fe04358d09fd79a235a5f3c48803a15c
Pumeo <nhunghn1010@gmail.com>
2018-06-22 13:28:54 +0700

    exercise

--- ScaleUtility.js
+++
@@ -1,9 +0,0 @@
-import { Dimensions } from 'react-native';
-
-const IPHONE_8_DEVICE_WIDTH = 414;
-
-export default class ScaleUtility {
-  static getSize(size) {
-    return (size * Dimensions.get('window').width) / IPHONE_8_DEVICE_WIDTH;
-  }
-}
